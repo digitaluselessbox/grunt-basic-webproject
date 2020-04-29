ed68a9f339e46a7c6a08eb0a32ba5f93dd8ee82f
Michael Klippel <leppilkm@gmail.com>
2020-04-29 23:20:13 +0200

    * update readme * update gitignore * cleanup extensions * new extension grunt-banner * insert "hello world!" example

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,15 +3,39 @@ module.exports = function (grunt) {
     const sass = require('node-sass');
 
     require('jit-grunt')(grunt);
-    
+	grunt.loadNpmTasks('grunt-banner');
+	
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         customs: grunt.file.readJSON('gruntfile_config.custom.json'),
 		
 		meta: {
-            copyright: 'Copyright (c) 2020-<%= grunt.template.today("yyyy") %>'
+            copyright: 'Copyright (c) 2019-<%= grunt.template.today("yyyy") %>',
+            banner: '\n' +
+                ' * <%= pkg.name %>\n' +
+                ' * Version: <%= pkg.version %>\n' +
+                ' * \n' +
+                ' * <%= meta.copyright %>\n' +
+                ' * by <%= pkg.author.name %>\n' +
+                ' * \n' +
+                ' * Street: <%= pkg.author.street %>\n' +
+                ' * City: <%= pkg.author.city %>\n' +
+                ' * Mail: <<%= pkg.author.email %>>\n' +
+                ' * \n' +
+                ' * Licensed under the <%= pkg.license %> License.\n'
         },
-        
+		usebanner: {
+            production: {
+                options: {
+                    position: 'top',
+                    banner: '/* \n * <%= meta.banner %>*/\n',
+                    linebreak: true
+                },
+                files: {
+                    src: [ 'dist/css/main.css' ]
+                }
+            }
+        },
         sass: {
             options: {
                 implementation: sass,
@@ -25,8 +49,7 @@ module.exports = function (grunt) {
             },
             production: {
                 files: {  // Dictionary of files
-                    'css/main.css': 'scss/main.scss',
-                    
+                    'dist/css/main.css': 'scss/main.scss', 
                 }
             }
         },
@@ -88,6 +111,6 @@ module.exports = function (grunt) {
     });
 
     grunt.registerTask('dev', ['sass:development', 'postcss:development', 'watch']);
-    grunt.registerTask('build', ['clean:production', 'sass:production', 'postcss:production', 'realFavicon:production', 'copy:production'] );
+    grunt.registerTask('build', ['clean:production', 'sass:production', 'postcss:production', 'usebanner:production', 'copy:production'] );
     grunt.registerTask('default', ['dev']);
 };
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-basic
+# Grunt Basic Web Project
+
+The idea is to create a basic web template I can take to easily start a new projekt powered by grunt.
+
+Actually it is used to create local overwrites for a website but in the future it also should be used to create normal websites.
+
+it includes 
+- css post-processors (https://www.npmjs.com/package/grunt-postcss)
+- sass css preprocessor scripting language (https://www.npmjs.com/package/grunt-sass)
+- file watcher for sass-files
+- a modular css compressor (https://www.npmjs.com/package/cssnano)
+- development deploy
+- production deploy
+- custom config with json
+- grunt just in time extention loader (jit-grunt)
+
+
+
